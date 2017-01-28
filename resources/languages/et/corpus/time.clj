d9c3d25579fed0618247bc0f686c73a2008c97cd
Mart Laus <mart.laus@ttu.ee>
2017-01-28 21:01:19 +0200

    Add estonian translations for time

--- resources/languages/et/corpus/time.clj
+++ resources/languages/et/corpus/time.clj
@@ -120,515 +120,513 @@
   "ülejärgmisel märtsil"
   (datetime 2014 3)
 
-  "Sunday, Feb 10"
+  "Pühapäev, 10. veebruar"
   (datetime 2013 2 10 :day-of-week 7 :day 10 :month 2) 
   
-  "Wed, Feb13"
+  "Kolmap, 13. veebr"
   (datetime 2013 2 13 :day-of-week 3 :day 13 :month 2)
 
-  "Monday, Feb 18"
-  "Mon, February 18"
+  "Esmaspäev, 18. veebruar"
+  "Esmasp, 18 veebruar"
   (datetime 2013 2 18 :day-of-week 1 :day 18 :month 2)
 
 ;   ;; Cycles
   
-  "this week"
-  "coming week"
+  "see nädal"
+  "sel nädalal"
   (datetime 2013 2 11 :grain :week)
 
-  "last week"
+  "eelmine nädal"
   (datetime 2013 2 4 :grain :week)
   
-  "next week"
+  "järgmine nädal"
   (datetime 2013 2 18 :grain :week)
   
-  "last month"
+  "eelmine kuu"
   (datetime 2013 1)
 
-  "next month"
+  "järgmine kuu"
   (datetime 2013 3)
   
-  "this quarter"
+  "see kvartal"
   (datetime 2013 1 1 :grain :quarter)
   
-  "next quarter"
+  "järgmine kvartal"
   (datetime 2013 4 1 :grain :quarter)
   
-  "third quarter"
+  "kolmas kvartal"
   (datetime 2013 7 1 :grain :quarter)
   
-  "4th quarter 2018"
+  "neljas kvartal 2018"
   (datetime 2018 10 1 :grain :quarter)
   
-  "last year"
+  "eelmine aasta"
   (datetime 2012)
   
-  "this year"
+  "see aasta"
   (datetime 2013)
   
-  "next year"
+  "järgmine aasta"
   (datetime 2014)
     
-  "last sunday"
-  "sunday from last week"
-  "last week's sunday"
+  "eelmine pühapäev"
+  "eelmise nädala pühapäev"
   (datetime 2013 2 10 :day-of-week 7)
 
-  "last tuesday"
+  "eelmine teisipäev"
   (datetime 2013 2 5 :day-of-week 2)
 
-  "next tuesday" ; when today is Tuesday, "mardi prochain" is a week from now
+  "järgmine teisipäev" ; when today is Tuesday, "mardi prochain" is a week from now
   (datetime 2013 2 19 :day-of-week 2)
 
-  "next wednesday" ; when today is Tuesday, "mercredi prochain" is tomorrow
+  "järgmine kolmapäev" ; when today is Tuesday, "mercredi prochain" is tomorrow
   (datetime 2013 2 13 :day-of-week 3)
 
-  "wednesday of next week"
-  "wednesday next week"
-  "wednesday after next"
+  "järgmise nädala kolmapäev"
+  "tulev kolmapäev"
   (datetime 2013 2 20 :day-of-week 3)
 
-  "friday after next"
+  "järgmine reede"
+  "tulev reede"
   (datetime 2013 2 22 :day-of-week 5)
 
-  "monday of this week"
+  "selle nädala esmaspäev"
   (datetime 2013 2 11 :day-of-week 1)
 
-  "tuesday of this week"
+  "selle nädala teisipäev"
   (datetime 2013 2 12 :day-of-week 2)
 
-  "wednesday of this week"
+  "selle nädala kolmapäev"
   (datetime 2013 2 13 :day-of-week 3)
 
-  "the day after tomorrow"
+  "ülehomme"
   (datetime 2013 2 14)
   
-  "the day before yesterday"
+  "üleile"
   (datetime 2013 2 10)
   
-  "last Monday of March"
+  "märtsi viimane esmaspäev"
   (datetime 2013 3 25 :day-of-week 1)
   
-  "last Sunday of March 2014"
+  "2014 aasta märtsi viimane pühapäev"
   (datetime 2014 3 30 :day-of-week 7)
   
-  "third day of october"
+  "oktoobri kolmas päev"
   (datetime 2013 10 3)
   
-  "first week of october 2014"
+  "2014 aasta oktoobri esimene nädal"
   (datetime 2014 10 6 :grain :week)
   
-  "last day of october 2015"
+  "2015 aasta oktoobri viimane päev"
   (datetime 2015 10 31)
   
-  "last week of september 2014"
+  "2014 aasta septembri viimane nädal"
   (datetime 2014 9 22 :grain :week)
   
   
   ;; nth of
-  "first tuesday of october"
+  "oktoobri esimene teisipäev"
   (datetime 2013 10 1)
   
-  "third tuesday of september 2014"
+  "2014 aasta septembri kolmas teisipäev"
   (datetime 2014 9 16)
   
-  "first wednesday of october 2014"
+  "2014 aasta oktoobri esimene teisipäev"
   (datetime 2014 10 1)
   
-  "second wednesday of october 2014"
+  "2014 aasta oktoobri teine kolmapäev"
   (datetime 2014 10 8)
   
   ;; nth after
   
-  "third tuesday after christmas 2014"
+  "kolmas teisipäev pärast 2014 aasta jõule"
   (datetime 2015 1 13)
     
 
   ;; Hours
   
-  "at 3am"
-  "at 3 AM"
-  "3 oclock am"
-  "at three am"
+  "kell 3 öösel"
+  "kell 3"
   (datetime 2013 2 13 3)
   
-  "3:18am"
-  "3:18a"
+  "3.18"
+  "03.18"
   (datetime 2013 2 12 3 18)
   
-  "at 3pm"
-  "@ 3pm"
-  "3PM"
-  "3pm"
-  "3 oclock pm"
-  "3 o'clock in the afternoon"
+  "kell kolm päeval"
+  "kell 3 päeval"
+  "kell 15.00 päeval"
+  "kell 3 pärastlõunal"
+  "kell 15.00 pärastlõunal"
+  "15.00"
   (datetime 2013 2 12 15 :hour 3 :meridiem :pm)
 
-  "3ish pm"
-  "3pm approximately"
-  "at about 3pm"
+  "kolme paiku"
+  "kolme ajal"
+  "umbes kolm"
   (datetime 2013 2 12 15 :hour 3 :meridiem :pm :precision "approximate")
 
-  "tomorrow 5pm sharp"
+  "homme täpselt viiest"
   (datetime 2013 2 13 17 :hour 5 :meridiem :pm :precision "exact")
 
-  "at 15 past 3pm"
-  "a quarter past 3pm"
-  "3:15 in the afternon"
-  "15:15"
-  "3:15pm"
-  "3:15PM"
-  "3:15p"
+  "15 minutit pärast kolme"
+  "veerand tundi pärast kolme"
+  "15.15 pärastlõunal"
+  "15.15"
   (datetime 2013 2 12 15 15 :hour 3 :minute 15 :meridiem :pm)
 
-  "at 20 past 3pm"
-  "3:20 in the afternoon"
-  "3:20 in afternoon"
-  "twenty after 3pm"
-  "3:20p"
+  "20 minutit pärast kolme"
+  "15.20 pärastlõunal"
+  "kakskümmend minutit pärast kolme"
   (datetime 2013 2 12 15 20 :hour 3 :minute 20 :meridiem :pm)
 
-  "at half past three pm"
-  "half past 3 pm"
-  "15:30"
-  "3:30pm"
-  "3:30PM"
-  "330 p.m."
-  "3:30 p m"
+  "pool tundi pärast kolme"
+  "15.30"
   (datetime 2013 2 12 15 30 :hour 3 :minute 30 :meridiem :pm)
   
-  "3:30"
-  "half three"
+  "15.30"
+  "pool neli"
   (datetime 2013 2 12 15 30 :hour 3 :minute 30)
 
-  "a quarter to noon"
-  "11:45am"
-  "15 to noon" ; Ambiguous with interval
+  "veerand tundi enne lõunat"
+  "15 minutit enne lõunat"
+  "11.45"
   (datetime 2013 2 12 11 45 :hour 11 :minute 45)
     
-  "8 tonight"
-  "eight tonight"
-  "8 this evening"
+  "20.00"
+  "täna kell kaheksa"
+  "täna kell 8"
+  "täna õhtul kaheksast"
+  "täna õhtul kell kaheksa"
   (datetime 2013 2 12 20)
  
   ;; Mixing date and time
     
-  "at 7:30 PM on Fri, Sep 20"
+  "19.30 reedel 20 septembril"
+  "19.30 reedel kahekümnendal septembril"
   (datetime 2013 9 20 19 30 :hour 7 :minute 30 :meridiem :pm)
   
-  "at 9am on Saturday"
+  "kell üheksa laupäeval"
+  "kell üheksa hommikul laupäeval"
+  "kell üheksa laupäeva hommikul"
   (datetime 2013 2 16 9 :day-of-week 6 :hour 9 :meridiem :am)
 
-  "Fri, Jul 18, 2014 07:00 PM"
+  "Reede, 18. juuli 2014 19.00"
   (datetime 2014 7 18 19 0 :day-of-week 5 :hour 7 :meridiem :pm)
 
   
 ; ;; Involving periods  
   
-  "in a sec"
-  "one second from now"
+  "sekundi pärast"
+  "ühe sekundi pärast"
   (datetime 2013 2 12 4 30 1)
   
-  "in a minute"
-  "in one minute"
+  "minuti pärast"
+  "ühe minuti pärast"
   (datetime 2013 2 12 4 31 0)
   
-  "in 2 minutes"
-  "in 2 more minutes"
-  "2 minutes from now"
+  "kahe minuti pärast"
   (datetime 2013 2 12 4 32 0)
   
-  "in 60 minutes"
+  "kuuekümne minuti pärast"
+  "60 minuti pärast"
   (datetime 2013 2 12 5 30 0)
   
-  "in half an hour"
+  "poole tunni pärast"
   (datetime 2013 2 12 5 0 0)
 
-  "in 2.5 hours"
-  "in 2 and an half hours"
+  "kahe ja poole tunni pärast"
+  "2.5h pärast"
   (datetime 2013 2 12 7 0 0)
 
   "in one hour"
   (datetime 2013 2 12 5 30)
   
-  "in a couple hours"
-  "in a couple of hours"
+  "paari tunni pärast"
   (datetime 2013 2 12 6 30)
 
-  "in a few hours"
-  "in few hours"
+  "mõne tunni pärast"
   (datetime 2013 2 12 7 30)
 
-  "in 24 hours"
+  "kahekümne nelja tunni pärast"
+  "24h pärast"
   (datetime 2013 2 13 4 30)
   
-  "in a day"
-  "a day from now"
+  "päeva pärast"
   (datetime 2013 2 13 4)
 
-  "3 years from today"
+  "kolme aasta pärast"
   (datetime 2016 2)
 
-  "in 7 days"
+  "seitsme päeva pärast"
+  "7 päeva pärast"
   (datetime 2013 2 19 4)
 
-  "in 1 week"
-  "in a week"
+  "ühe nädala pärast"
+  "nädala pärast"
   (datetime 2013 2 19)
 
-  "in about half an hour"
+  "umbes poole tunni pärast"
   (datetime 2013 2 12 5 0 0 :precision "approximate")
 
-  "7 days ago"
+  "seitse päeva tagasi"
   (datetime 2013 2 5 4)
 
-  "14 days ago"
-  "a fortnight ago"
+  "14 päeva tagasi"
+  "neliteist päeva tagasi"
+  "kaks nädalat tagasi"
   (datetime 2013 1 29 4)
   
-  "a week ago"
-  "one week ago"
-  "1 week ago"
+  "nädal tagasi"
+  "üks nädal tagasi"
+  "1 nädal tagasi"
   (datetime 2013 2 5)
 
-  "three weeks ago"
+  "kolm nädalat tagasi"
   (datetime 2013 1 22)
   
-  "three months ago"
+  "kolm kuud tagasi"
   (datetime 2012 11 12)
 
-  "two years ago"
+  "kaks aastat tagasi"
   (datetime 2011 2)
   
-  "7 days hence"
+  "7 päeva pärast"
   (datetime 2013 2 19 4)
 
-  "14 days hence"
-  "a fortnight hence"
+  "14 päeva pärast"
+  "kahe nädala pärast"
   (datetime 2013 2 26 4)
   
-  "a week hence"
-  "one week hence"
-  "1 week hence"
+  "nädala pärast"
+  "ühe nädala pärast"
   (datetime 2013 2 19)
 
-  "three weeks hence"
+  "kolme nädala pärast"
   (datetime 2013 3 5)
   
-  "three months hence"
+  "kolme kuu pärast"
   (datetime 2013 5 12)
 
-  "two years hence"
+  "kahe aasta pärast"
   (datetime 2015 2)
   
-  "one year after christmas"
+  "üks aasta pärast jõule"
+  "aasta pärast jõule"
   (datetime 2013 12) ; resolves as after last Xmas...
   
 
   ; Seasons
 
-  "this summer"
+  "see suvi"
+  "tulev suvi"
   (datetime-interval [2013 6 21] [2013 9 24])
 
-  "this winter"
+  "see talv"
+  "tulev talv"
   (datetime-interval [2012 12 21] [2013 3 21])
 
   ; US holidays (http://www.timeanddate.com/holidays/us/)
 
-  "xmas"
-  "christmas"
-  "christmas day"
+  "jõulud"
+  "jõululaupäev"
   (datetime 2013 12 25)
 
-  "new year's eve"
-  "new years eve"
+  "vana-aasta õhtu"
   (datetime 2013 12 31)
 
-  "new year's day"
-  "new years day"
+  "esimene jaanuar"
   (datetime 2014 1 1)
 
-  "valentine's day"
-  "valentine day"
+  "sõbrapäev"
+  "valentinipäev"
   (datetime 2013 2 14)
 
-  "memorial day"
-  (datetime 2013 5 27)
+;  "mälestamise päev"
+;  (datetime 2013 5 27)
 
-  "Mother's Day"
+  "emadepäev"
   (datetime 2013 5 12)
   
-  "Father's Day"
+  "isadepäev"
   (datetime 2013 6 16)
 
-  "memorial day week-end"
-  (datetime-interval [2013 5 24 18] [2013 5 28 0])
+;  "memorial day week-end"
+;  (datetime-interval [2013 5 24 18] [2013 5 28 0])
 
-  "independence day"
-  "4th of July"
-  "4 of july"
-  (datetime 2013 7 4)
-
-  "labor day"
-  (datetime 2013 9 2)
+;  "independence day"
+;  "4th of July"
+;  "4 of july"
+;  (datetime 2013 7 4)
+;
+;  "labor day"
+;  (datetime 2013 9 2)
 
   "halloween"
   (datetime 2013 10 31)
 
-  "thanksgiving day"
-  "thanksgiving"
-  (datetime 2013 11 28)
+;  "thanksgiving day"
+;  "thanksgiving"
+;  (datetime 2013 11 28)
   
   ; Part of day (morning, afternoon...)
   
-  "this evening"
-  "today evening"
-  "tonight"
+  "õhtul"
+  "täna õhtul"
+  "täna"
   (datetime-interval [2013 2 12 18] [2013 2 13 00])
 
-  "tomorrow evening"
-  ;"Wednesday evening"
-  "tomorrow night"
+  "homme õhtul"
   (datetime-interval [2013 2 13 18] [2013 2 14 00])
   
-  "yesterday evening"
+  "eile õhtul"
   (datetime-interval [2013 2 11 18] [2013 2 12 00])
     
-  "this week-end"
+  "sel nädalavahetusel"
+  "sel nv-l"
+  "sel nvl"
   (datetime-interval [2013 2 15 18] [2013 2 18 00])
 
-  "monday morning"
+  "esmaspäeva hommikul"
   (datetime-interval [2013 2 18 4] [2013 2 18 12])
 
-  "february the 15th in the morning"
-  "15 of february in the morning"
-  "morning of the 15th of february"
+  "15. veebruari hommik"
+  "15 veebruari hommik"
+  "viieteistkümnenda veebruari hommik"
   (datetime-interval [2013 2 15 4] [2013 2 15 12])
 
   
   ; Intervals involving cycles
   
-  "last 2 seconds"
-  "last two seconds"
+  "viimased 2 sekundit"
+  "viimased kaks sekundit"
   (datetime-interval [2013 2 12 4 29 58] [2013 2 12 4 30 00])
 
-  "next 3 seconds"
-  "next three seconds"
+  "järgmised kolm sekundit"
+  "järgmised 3 sekundit"
   (datetime-interval [2013 2 12 4 30 01] [2013 2 12 4 30 04])
 
-  "last 2 minutes"
-  "last two minutes"
+  "viimased kaks minutit"
+  "viimased 2 minutit"
   (datetime-interval [2013 2 12 4 28] [2013 2 12 4 30])
 
-  "next 3 minutes"
-  "next three minutes"
+  "järgmised 3 minutit"
+  "järgmised kolm minutit"
   (datetime-interval [2013 2 12 4 31] [2013 2 12 4 34])
 
-  "last 1 hour"
-  "last one hour"
+  "viimane tund"
   (datetime-interval [2013 2 12 3] [2013 2 12 4])
 
-  "next 3 hours"
-  "next three hours"
+  "järgmised 3 tundi"
+  "järgmised kolm tundi"
   (datetime-interval [2013 2 12 5] [2013 2 12 8])
 
-  "last 2 days"
-  "last two days"
-  "past 2 days"
+  "viimased kaks päeva"
+  "viimased 2 päeva"
+  "möödunud 2 päeva"
+  "möödunud kaks päeva"
+  "eelmised 2 päeva"
+  "eelmised kaks päeva"
   (datetime-interval [2013 2 10] [2013 2 12])
 
-  "next 3 days"
-  "next three days"
+  "järgmised 3 päeva"
+  "järgmised kolm päeva"
   (datetime-interval [2013 2 13] [2013 2 16])
     
-  "next few days"
+  "järgmised paar päeva"
   (datetime-interval [2013 2 13] [2013 2 16])
 
-  "last 2 weeks"
-  "last two weeks"
-  "past 2 weeks"
+  "viimased 2 nädalat"
+  "viimased kaks nädalat"
+  "möödunud 2 nädalat"
+  "möödunud kaks nädalat"
+  "eelmised 2 nädalat"
+  "eelmised kaks nädalat"
   (datetime-interval [2013 1 28 :grain :week] [2013 2 11 :grain :week])
 
-  "next 3 weeks"
-  "next three weeks"
+  "järgmised 3 nädalat"
+  "järgmised kolm nädalat"
   (datetime-interval [2013 2 18 :grain :week] [2013 3 11 :grain :week])
 
-  "last 2 months"
-  "last two months"
+  "viimased 2 kuud"
+  "viimased kaks kuud"
   (datetime-interval [2012 12] [2013 02])
 
-  "next 3 months"
-  "next three months"
+  "järgmised 3 kuud"
+  "järgmised kolm kuud"
   (datetime-interval [2013 3] [2013 6])
 
-  "last 2 years"
-  "last two years"
+  "viimased 2 aastat"
+  "viimased kaks aastat"
   (datetime-interval [2011] [2013])
   
-  "next 3 years"
-  "next three years"
+  "järgmised kolm aastat"
+  "järgmised 3 aastat"
   (datetime-interval [2014] [2017])
 
   
   ; Explicit intervals
 
-  "July 13-15"
-  "July 13 to 15"
-  "July 13 thru 15"
-  "July 13 through 15"
-  "July 13 - July 15"
+  "13.-15. juuli"
+  "13. kuni 15. juuli"
+  "13. juuli kuni 15. juuli"
+  "13. juuli - 15. juuli"
   (datetime-interval [2013 7 13] [2013 7 16])
 
-  "Aug 8 - Aug 12"
+  "8. august - 12. august"
   (datetime-interval [2013 8 8] [2013 8 13])
   
-  "9:30 - 11:00"
+  "9.30 - 11.00"
   (datetime-interval [2013 2 12 9 30] [2013 2 12 11 1])
 
-  "from 9:30 - 11:00 on Thursday"
-  "between 9:30 and 11:00 on thursday"
-  "9:30 - 11:00 on Thursday"
-  "later than 9:30 but before 11:00 on Thursday"
-  "Thursday from 9:30 to 11:00"
+  "9.30 - 11.00 neljapäeval"
+  "9.30 kuni 11.00 neljapäeval"
+  "alates 9.30 kuni 11.00 neljapäeval"
   (datetime-interval [2013 2 14 9 30] [2013 2 14 11 1])
 
-  "Thursday from 9a to 11a" 
+  "Neljapäeval üheksast üheteistkümneni"
+  "Neljapäeval 9.00 kuni 11.00"
+  "Neljapäeval 9.00 - 11.00"
   (datetime-interval [2013 2 14 9] [2013 2 14 12])
   
-  "11:30-1:30" ; go train this rule!
-  "11:30-1:30"
-  "11:30-1:30"
-  "11:30-1:30"
-  "11:30-1:30"
-  "11:30-1:30"
-  "11:30-1:30"
+  "11.30-13.30" ; go train this rule!
+  "11.30-13.30"
+  "11.30-13.30"
+  "11.30-13.30"
+  "11.30-13.30"
+  "11.30-13.30"
+  "11.30-13.30"
   (datetime-interval [2013 2 12 11 30] [2013 2 12 13 31])
 
-  "1:30 PM on Sat, Sep 21"
+  "13.30 laupäeval, 21. septembril"
   (datetime 2013 9 21 13 30)
   
-  "within 2 weeks"
+  "kahe nädala jooksul"
   (datetime-interval [2013 2 12 4 30 0] [2013 2 26])
 
-  "until 2:00pm"
+  "kuni kaheni"
+  "kuni 14.00"
+  "kuni kella kaheni"
   (datetime-interval [2013 2 12 4 30 0] [2013 2 12 14])
 
-  "by 2:00pm"
+  "kaheks"
+  "kella kaheks"
   (datetime-interval [2013 2 12 4 30 0] [2013 2 12 14])
 
-  "by EOD"
+  "päeva lõõpuks"
   (datetime-interval [2013 2 12 4 30 0] [2013 2 13 0])
 
-  "by EOM"
+  "kuu lõpuks"
   (datetime-interval [2013 2 12 4 30 0] [2013 3 1 0])
 
-  "by the end of next month"
+  "järgmise kuu lõpuks"
   (datetime-interval [2013 2 12 4 30 0] [2013 4 1 0])
   ; Timezones
   
-  "4pm CET"
+  "kell neli CET"
+  "kell 4 CET"
   (datetime 2013 2 12 16 :hour 4 :meridiem :pm :timezone "CET")
 
-  "Thursday 8:00 GMT"
+  "neljapäeval 8:00 GMT"
   (datetime 2013 2 14 8 00 :timezone "GMT")  
 )
